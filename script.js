6deb7f6f29cbb5375a9f66d7fbe0be143ffcc0ed
kelvin-ux <245871@edu.p.lodz.pl>
2024-05-18 16:22:29 +0200

    adjustments to the main page

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("test");
+const textElement = document.getElementById('animated-text');
+    const originalText = textElement.textContent;
+
+    function restartAnimation() {
+        textElement.classList.remove('animated-text');
+        void textElement.offsetWidth; 
+        textElement.classList.add('animated-text');
+    }
+
+    setInterval(restartAnimation, 10000); // 10 s delay to rewrite #first
