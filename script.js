6761af887d597521ee005fd5ebb0012719077369
kelvin-ux <245871@edu.p.lodz.pl>
2024-05-19 13:20:23 +0200

    poprawki strony glownej

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const textElement = document.getElementById('animated-text');
-    const originalText = textElement.textContent;
+const originalText = textElement.textContent;
+const refreshRateText = 15000;
+const refreshRateImage = 5000;
 
-    function restartAnimation() {
-        textElement.classList.remove('animated-text');
-        void textElement.offsetWidth; 
-        textElement.classList.add('animated-text');
-    }
+function restartAnimation() {
+    textElement.classList.remove('animated-text');
+    void textElement.offsetWidth;
+    textElement.classList.add('animated-text');
+}
 
-    setInterval(restartAnimation, 10000); // 10 s delay to rewrite #first
+setInterval(restartAnimation, refreshRateText); // 15 s delay to rewrite #first
+
+// Background image carousel
+const images = [
+    'images/poi1.jpg',
+    'images/poi2.jpg',
+    'images/poi3.jpg'
+];
+let currentIndex = 0;
+const rightContent = document.querySelector('.right-content');
+
+function changeBackgroundImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    rightContent.style.backgroundImage = `url(${images[currentIndex]})`;
+}
+
+setInterval(changeBackgroundImage, refreshRateImage); 
+
+rightContent.style.backgroundImage = `url(${images[0]})`;
