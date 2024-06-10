bbe7562c8b76ed5527cde16d619418ea3b4bc0e3
Michal Urbaniak <245954@edu.p.lodz.pl>
2024-06-10 21:46:53 +0200

    upgrade ogloszen

--- subsites/info.php
+++ subsites/info.php
@@ -26,24 +26,53 @@
             <h1>Ogłoszenia</h1>
             <div class="announcement-list">
                 <div class="announcement">
-                    <img src="../blogimg/s1.png" alt="Dni Otwarte" class="announcement-img">
+                    <img src="../blogimg/juwe.jpg" alt="Juwenalia 2024" class="announcement-img">
                     <div class="announcement-text">
-                        <h2>Dni Otwarte na PŁ</h2>
-                        <p>Zapraszamy wszystkich zainteresowanych na Dni Otwarte Politechniki Łódzkiej! Odbędą się one 15 i 16 czerwca. Będzie to doskonała okazja, aby poznać naszą uczelnię, zwiedzić kampus oraz dowiedzieć się więcej o kierunkach studiów, które oferujemy. Przygotowaliśmy wiele atrakcji, w tym spotkania z wykładowcami i studentami, prezentacje laboratoriów oraz pokazy naukowe. Do zobaczenia!</p>
+                        <h2>Juwenalia 2024</h2>
+                        <p>Największe studenckie święto na uczelni, pełne koncertów, imprez plenerowych, konkursów i wspólnej zabawy. Odbędzie się 13 maja 2024 roku.</p>
                     </div>
                 </div>
                 <div class="announcement">
-                    <img src="../blogimg/s1.png" alt="Charytatywny Maraton" class="announcement-img">
+                    <img src="../blogimg/tragii.jpeg" alt="Targi Pracy 2024" class="announcement-img">
                     <div class="announcement-text">
-                        <h2>Charytatywny Maraton PŁ</h2>
-                        <p>Politechnika Łódzka organizuje charytatywny maraton, który odbędzie się 25 czerwca. Wszystkie zebrane środki zostaną przekazane na wsparcie lokalnych organizacji charytatywnych. Biegniemy, aby pomóc potrzebującym! Maraton rozpocznie się o godzinie 10:00 na głównym stadionie uczelni. Zapisy trwają do 20 czerwca. Dołącz do nas i biegnij dla dobra innych!</p>
+                        <h2>Targi Pracy 2024</h2>
+                        <p>Zapraszamy na Targi Pracy, które odbędą się 21 marca 2024 roku. Doskonała okazja do znalezienia pracy lub stażu.</p>
                     </div>
                 </div>
                 <div class="announcement">
-                    <img src="../blogimg/s1.png" alt="Konkurs Informatyczny" class="announcement-img">
+                    <img src="../blogimg/festw.jpeg" alt="Festiwal Nauki i Techniki 2024" class="announcement-img">
                     <div class="announcement-text">
-                        <h2>Konkurs Informatyczny PŁ</h2>
-                        <p>Zapraszamy wszystkich studentów do udziału w Konkursie Informatycznym Politechniki Łódzkiej! Konkurs odbędzie się 30 czerwca i będzie składał się z kilku etapów, w tym zadań programistycznych oraz prezentacji projektów. Na zwycięzców czekają atrakcyjne nagrody, w tym staże w wiodących firmach IT oraz sprzęt komputerowy. Rejestracja trwa do 25 czerwca. Pokaż swoje umiejętności i zdobądź cenne doświadczenie!</p>
+                        <h2>Festiwal Nauki i Techniki 2024</h2>
+                        <p>Zapraszamy na Festiwal Nauki i Techniki, który odbędzie się 9 września 2023 roku. Wiele ciekawych wykładów i prezentacji.</p>
+                    </div>
+                </div>
+                <div class="announcement">
+                    <img src="../blogimg/kong.png" alt="Kongres Innowacji Sierpien 2023" class="announcement-img">
+                    <div class="announcement-text">
+                        <h2>Kongres Innowacji Sierpień 2023</h2>
+                        <p>VI Kongres Innowacji w Energetyce odbędzie się 17 sierpnia 2023 roku. Dołącz do nas i poznaj najnowsze innowacje.</p>
+                    </div>
+                </div>
+                <div class="announcement">
+                    <img src="../blogimg/dn.jpg" alt="Dni Otwarte 2024" class="announcement-img">
+                    <div class="announcement-text">
+                        <h2>Dni Otwarte 2024</h2>
+                        <p>Dni Otwarte Politechniki Łódzkiej odbędą się 14 marca 2024 roku. Zapraszamy wszystkich zainteresowanych studiami na naszej uczelni.</p>
+                    </div>
+                </div>
+                <!-- Nowe ogłoszenia -->
+                <div class="announcement">
+                    <img src="../images/warsztaty.jpg" alt="Warsztaty Programistyczne" class="announcement-img">
+                    <div class="announcement-text">
+                        <h2>Warsztaty Programistyczne</h2>
+                        <p>Zapraszamy na warsztaty programistyczne, które odbędą się 10 lipca. Będzie to doskonała okazja, aby nauczyć się podstaw programowania oraz poznać najnowsze technologie. Warsztaty są otwarte dla wszystkich studentów.</p>
+                    </div>
+                </div>
+                <div class="announcement">
+                    <img src="../images/wystawa.jpg" alt="Wystawa Projektów Studenckich" class="announcement-img">
+                    <div class="announcement-text">
+                        <h2>Wystawa Projektów Studenckich</h2>
+                        <p>Zapraszamy na wystawę projektów studenckich, która odbędzie się 5 sierpnia. Studenci zaprezentują swoje projekty z różnych dziedzin, w tym inżynierii, informatyki i sztuki. Przyjdź i zobacz, nad czym pracują twoi koledzy!</p>
                     </div>
                 </div>
             </div>
