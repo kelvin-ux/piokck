1770c3068a48a95d99d097272f25f346ecba9783
Michal Urbaniak <245954@edu.p.lodz.pl>
2024-06-11 19:29:51 +0200

    caly backend do ogloszen

--- subsites/info.php
+++ subsites/info.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
-    <?php session_start(); ?>
+    <?php include '../log/db.php'; ?>
+
     <header>
         <nav>
             <ul class="nav-left">
@@ -18,6 +19,14 @@
                 <li><a href="korepetycje.php">Korki</a></li>
                 <li><a href="metody.php">Metody Nauczania</a></li>
             </ul>
+            <ul class="nav-right">
+                <?php
+                session_start();
+                if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION['typ'] == 1) {
+                    echo "<li><a href='edycja_ogloszen.php'>Edycja Ogłoszeń</a></li>";
+                }
+                ?>
+            </ul>
         </nav>
     </header>
 
@@ -25,61 +34,28 @@
         <section id="announcements">
             <h1>Ogłoszenia</h1>
             <div class="announcement-list">
-                <div class="announcement">
-                    <img src="../blogimg/juwe.jpg" alt="Juwenalia 2024" class="announcement-img">
-                    <div class="announcement-text">
-                        <h2>Juwenalia 2024</h2>
-                        <p>Największe studenckie święto na uczelni, pełne koncertów, imprez plenerowych, konkursów i wspólnej zabawy. Odbędzie się 13 maja 2024 roku.</p>
-                    </div>
-                </div>
-                <div class="announcement">
-                    <img src="../blogimg/tragii.jpeg" alt="Targi Pracy 2024" class="announcement-img">
-                    <div class="announcement-text">
-                        <h2>Targi Pracy 2024</h2>
-                        <p>Zapraszamy na Targi Pracy, które odbędą się 21 marca 2024 roku. Doskonała okazja do znalezienia pracy lub stażu.</p>
-                    </div>
-                </div>
-                <div class="announcement">
-                    <img src="../blogimg/festw.jpeg" alt="Festiwal Nauki i Techniki 2024" class="announcement-img">
-                    <div class="announcement-text">
-                        <h2>Festiwal Nauki i Techniki 2024</h2>
-                        <p>Zapraszamy na Festiwal Nauki i Techniki, który odbędzie się 9 września 2023 roku. Wiele ciekawych wykładów i prezentacji.</p>
-                    </div>
-                </div>
-                <div class="announcement">
-                    <img src="../blogimg/kong.png" alt="Kongres Innowacji Sierpien 2023" class="announcement-img">
-                    <div class="announcement-text">
-                        <h2>Kongres Innowacji Sierpień 2023</h2>
-                        <p>VI Kongres Innowacji w Energetyce odbędzie się 17 sierpnia 2023 roku. Dołącz do nas i poznaj najnowsze innowacje.</p>
-                    </div>
-                </div>
-                <div class="announcement">
-                    <img src="../blogimg/dn.jpg" alt="Dni Otwarte 2024" class="announcement-img">
-                    <div class="announcement-text">
-                        <h2>Dni Otwarte 2024</h2>
-                        <p>Dni Otwarte Politechniki Łódzkiej odbędą się 14 marca 2024 roku. Zapraszamy wszystkich zainteresowanych studiami na naszej uczelni.</p>
-                    </div>
-                </div>
-                <!-- Nowe ogłoszenia -->
-                <div class="announcement">
-                    <img src="../images/warsztaty.jpg" alt="Warsztaty Programistyczne" class="announcement-img">
-                    <div class="announcement-text">
-                        <h2>Warsztaty Programistyczne</h2>
-                        <p>Zapraszamy na warsztaty programistyczne, które odbędą się 10 lipca. Będzie to doskonała okazja, aby nauczyć się podstaw programowania oraz poznać najnowsze technologie. Warsztaty są otwarte dla wszystkich studentów.</p>
-                    </div>
-                </div>
-                <div class="announcement">
-                    <img src="../images/wystawa.jpg" alt="Wystawa Projektów Studenckich" class="announcement-img">
-                    <div class="announcement-text">
-                        <h2>Wystawa Projektów Studenckich</h2>
-                        <p>Zapraszamy na wystawę projektów studenckich, która odbędzie się 5 sierpnia. Studenci zaprezentują swoje projekty z różnych dziedzin, w tym inżynierii, informatyki i sztuki. Przyjdź i zobacz, nad czym pracują twoi koledzy!</p>
-                    </div>
-                </div>
+                <?php
+                $sql = "SELECT tytul, Opis, Zdjecie FROM ogloszenia";
+                $result = $mysqli->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<div class='announcement'>";
+                        echo "<img src='" . $row['Zdjecie'] . "' alt='" . $row['tytul'] . "' class='announcement-img'>";
+                        echo "<div class='announcement-text'>";
+                        echo "<h2>" . $row['tytul'] . "</h2>";
+                        echo "<p>" . substr($row['Opis'], 0, 200) . "...</p>";
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                } else {
+                    echo "Brak dostępnych ogłoszeń.";
+                }
+                ?>
             </div>
         </section>
     </div>
 
     <footer>&copy; 2024 Poly. All rights reserved</footer>
-    <script src='../script.js'></script>
 </body>
 </html>
